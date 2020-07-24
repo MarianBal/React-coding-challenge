edcd7048e6688506f907da057c8435678f1db16b
Mariana Bálsamo <mariana.balsamo@intive.com>
2020-07-24 14:51:46 -0300

    fetching data

--- src/components/MainApp/MainAppView.js
+++ src/components/MainApp/MainAppView.js
@@ -1,4 +1,6 @@
 import React from 'react';
 
-const MainAppWrapper = () => <h1>mainAppView</h1>;
-export default MainAppWrapper;
+const MainAppView = ({ movies, error }) => {
+  return <>{error ? <h1>Error</h1> : <h1>No Error</h1>}</>;
+};
+export default MainAppView;
