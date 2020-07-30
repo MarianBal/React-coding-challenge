408a7bd015fb80b289c4f1c0e43366fde6ca8352
Mariana Bálsamo <mariana.balsamo@intive.com>
2020-07-30 15:02:16 -0300

    last minute changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,9 +3,9 @@ import { endpoints, url } from '../constants/url';
 
 const apiURLConstructor = baseURL => extension => baseURL + extension;
 let baseUrl = url; //DEFAULT URL IS DEVELOPMENT
-if (window._CONFIG_) {
-  baseUrl = window._CONFIG_.API_URL;
-}
+// if (window._CONFIG_) {
+//   baseUrl = window._CONFIG_.API_URL;
+// }
 const moviesApi = apiURLConstructor(baseUrl);
 
 export const getMovies = () => () => {
